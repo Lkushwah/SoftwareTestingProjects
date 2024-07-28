fd12373aa7858d333d2d388ad15596dd8c8e4b36
Laharsh Kushwah <LaharshKushwah@gmail.com>
2024-07-28 17:59:57 +0530

    Files added

--- src/ex_21072024/lab031.java
+++ src/ex_21072024/lab031.java
@@ -8,8 +8,11 @@ public class lab031 {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
 
-        if(year %4 == 0){
-            System.out.println(year+ "is a leap year");}
+        if (year % 4 == 0) {
+            if (year % 400 == 0) {
+                System.out.println("leap year");
+            }
 
+        }
     }
 }
